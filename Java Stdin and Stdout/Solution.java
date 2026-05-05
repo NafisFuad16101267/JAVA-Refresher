c96dd001a07758fe690180ea4a828529c2af4a3d
Nafis <nafisfuadishere@gmail.com>
2026-05-05 14:10:27 +0600

    Added the solution file for Java Stdin and Stdout

---
+++ Java Stdin and Stdout/Solution.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int inputNumber1 = scan.nextInt();
+        int inputNumber2 = scan.nextInt();
+        int inputNumber3 = scan.nextInt();
+
+        System.out.println(inputNumber1);
+        System.out.println(inputNumber2);
+        System.out.println(inputNumber3);
+    }
+}
