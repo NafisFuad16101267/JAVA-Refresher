94c10eee74b91f63c5299cb3c36a427899f5a2e1
Nafis <nafisfuadishere@gmail.com>
2026-05-20 11:24:29 +0600

    Modified solution file for Java Sort practice problem

--- Java Sort/Checker.java
+++ Java Sort/Checker.java
@@ -5,11 +5,14 @@ public class Checker implements Comparator<Student>{
     @Override
     public int compare(Student student1, Student student2){
         if(student1.cgpa == student2.cgpa){
-            if(student1.fname.compareTo(student2.fname) == 0)
+            if(student1.fname.compareTo(student2.fname) == 0){
                 return student1.id - student2.id;
-            else
+            } else {
                 return (student1.fname.compareTo(student2.fname));
-        }
-        return (int)(student1.cpga - student2.cgpa);
+            }
+        } else if(student2.cgpa - student1.cgpa > 0)
+            return 1;
+        else
+            return -1;
     }
 }
--- Java Sort/Student.java
+++ Java Sort/Student.java
@@ -18,6 +18,6 @@ public class Student{
     }
 
     public double getCgpa(){
-        return cpga;
+        return cgpa;
     }
 }
