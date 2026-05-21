6737c3080569f100b9c59cfa9e634c10cefec1ea
Nafis <nafisfuadishere@gmail.com>
2026-05-21 17:47:12 +0600

    Add solution file for Java Deque practice problem

---
+++ Java Deque/Solution.java
@@ -0,0 +1,17 @@
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.Scanner;
+
+public class Solution{
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        int numberOfInputs = scanner.nextInt();
+        int subArraySize = scanner.nextInt();
+
+        Deque deque = new ArrayDeque<>();
+        
+        for(int index = 0; index < numberOfInputs; index++){
+            
+        }
+    }
+}
