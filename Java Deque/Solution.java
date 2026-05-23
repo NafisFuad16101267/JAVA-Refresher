c0d42e648501591f2e185e9f44b3cca9a147ad48
Nafis <nafisfuadishere@gmail.com>
2026-05-23 14:33:16 +0600

    Modify solution file for Java Deqeue practice problem

--- Java Deque/Solution.java
+++ Java Deque/Solution.java
@@ -1,17 +1,48 @@
-import java.util.ArrayDeque;
-import java.util.Deque;
-import java.util.Scanner;
+import java.util.*;
 
-public class Solution{
-    public static void main(String[] args){
+public class Solution {
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int numberOfInputs = scanner.nextInt();
+
+        int numberOfInput = scanner.nextInt();
         int subArraySize = scanner.nextInt();
 
-        Deque deque = new ArrayDeque<>();
-        
-        for(int index = 0; index < numberOfInputs; index++){
+        Deque<Integer> deque = new ArrayDeque<>();
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        int maxUnique = 0;
+        int subArrayCount = 0;
+
+        for (int index = 0; index < numberOfInput; index++) {
+            int inputNumber = scanner.nextInt();
+
+            deque.addLast(inputNumber);
             
+            if(!map.containsKey(inputNumber))
+                map.put(inputNumber, 1);
+            else
+                map.replace(inputNumber, map.get(inputNumber) + 1);
+
+            subArrayCount++;
+
+            if(subArrayCount == subArraySize){
+                int uniqueNumber = map.size();
+                maxUnique = (uniqueNumber > maxUnique)? uniqueNumber : maxUnique;
+                
+                int removedNumber = deque.removeFirst();
+                
+                if(map.get(removedNumber) == 1)
+                    map.remove(removedNumber);
+                else
+                    map.replace(removedNumber, map.get(removedNumber) - 1);
+                
+                subArrayCount--;
+            }
+
         }
+
+        scanner.close();
+
+        System.out.println(maxUnique);
     }
 }
